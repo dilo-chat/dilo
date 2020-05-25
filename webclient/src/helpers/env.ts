55399801d43400caf26f74329bad1e3e1fd9738e
jonathanmv <jonathan.morales@jimdo.com>
2020-05-25 18:52:00 +0200

    feat: added configuration interface

---
+++ webclient/src/helpers/env.ts
@@ -0,0 +1,25 @@
+export type ENV_VAR = "SERVER_URL"
+  | "SECURITY_ENCRYPTION_ALG_NAME"
+  | "SECURITY_DERIVE_ALG_NAME"
+  | "SECURITY_DERIVE_ALG_NAMED_CURVE";
+
+export type WebClientConfig = {
+  readonly [key in ENV_VAR]: string;
+};
+
+const env = (envvar: ENV_VAR): string => {
+  const value = process.env[envvar];
+  if (!value) {
+    throw new Error(`${envvar} was not set as an environment variable!`);
+  }
+  return value;
+}
+
+const config: WebClientConfig = Object.freeze<WebClientConfig>({
+  SERVER_URL: env('SERVER_URL'),
+  SECURITY_ENCRYPTION_ALG_NAME: env('SECURITY_ENCRYPTION_ALG_NAME'),
+  SECURITY_DERIVE_ALG_NAME: env('SECURITY_DERIVE_ALG_NAME'),
+  SECURITY_DERIVE_ALG_NAMED_CURVE: env('SECURITY_DERIVE_ALG_NAMED_CURVE'),
+});
+
+export default config;
