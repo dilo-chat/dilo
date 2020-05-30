54d65c2ca83208f0c0b287b5c324eda883a27f3f
jonathanmv <jonathan.morales@jimdo.com>
2020-05-30 09:09:41 +0200

    wip: generate public, private, and shared secret keys

--- lambdas/api/security.js
+++ lambdas/api/security.js
@@ -1,3 +1,24 @@
+const crypto = require('crypto')
+
 exports.enchangeKeys = (requestContext, { data, meta }) => {
+  const ecdh = generateKeyPair();
+  const remotePublicKey = meta.publicKey;
+  const sharedSecretKey = deriveSecretKey(ecdh, remotePublicKey);
+  const publicKey = ecdh.getPublicKey('utf8');
+  // store sharedSecretKey. It will be used to en/decrypt
+  // send publicKey so that remote can generate the sharedSecretKey
   
+}
+// https://security.stackexchange.com/a/78624
+// p-384 == secp384r1
+const generateKeyPair = () => {
+  const ecdh = crypto.createECDH('secp384r1');
+  ecdh.generateKeys();
+  return ecdh;
+}
+
+const encryptEvent
+const decryptEvent
+const deriveSecretKey = (ecdh, remotePublicKey) => {
+  return ecdh.computeSecret(remotePublicKey)
 }
