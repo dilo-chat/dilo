257fb7d46f8ed1dd89f87c5a7061afbba0c5deb5
jonathanmv <jonathan.morales@jimdo.com>
2020-05-30 09:26:03 +0200

    wip: update db record with shared key

--- lambdas/api/security.js
+++ lambdas/api/security.js
@@ -1,24 +1,30 @@
+const { put } = require('./storage');
 const crypto = require('crypto')
 
-exports.enchangeKeys = (requestContext, { data, meta }) => {
+const encoding = 'utf8'
+const format = 'uncompressed'
+
+exports.enchangeKeys = async({ connectionId }, { data, meta }) => {
   const ecdh = generateKeyPair();
   const remotePublicKey = meta.publicKey;
+  const roomId = meta.roomId;
   const sharedSecretKey = deriveSecretKey(ecdh, remotePublicKey);
-  const publicKey = ecdh.getPublicKey('utf8');
+  const publicKey = ecdh.getPublicKey(encoding);
   // store sharedSecretKey. It will be used to en/decrypt
   // send publicKey so that remote can generate the sharedSecretKey
-  
+  await put({ roomId, connectionId, sharedKey })
 }
+
 // https://security.stackexchange.com/a/78624
 // p-384 == secp384r1
 const generateKeyPair = () => {
   const ecdh = crypto.createECDH('secp384r1');
-  ecdh.generateKeys();
+  ecdh.generateKeys(encoding, format);
   return ecdh;
 }
 
 const encryptEvent
 const decryptEvent
 const deriveSecretKey = (ecdh, remotePublicKey) => {
-  return ecdh.computeSecret(remotePublicKey)
+  return ecdh.computeSecret(remotePublicKey, encoding, encoding);
 }
