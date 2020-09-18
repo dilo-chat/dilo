4288279e5e5ba38361d5fd2acb519b3c43bc42d7
jonathanmv <jonathan.morales@jimdo.com>
2020-09-19 00:08:34 +0100

    create stripe session on server

--- webclient/src/pages/Checkout.tsx
+++ webclient/src/pages/Checkout.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react'
+import { loadStripe } from '@stripe/stripe-js'
+
+const stripePromise = loadStripe(process.env.STRIPE_PUBLIC_KEY)
 
 export const Checkout = () => {
   return (
